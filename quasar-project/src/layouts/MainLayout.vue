8551b2f577e07c682229c085d6a0e387ba8513c4
Ana <algabg1@gmail.com>
2024-03-27 09:19:59 -0300

    new quasar project

---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Menu
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+defineOptions({
+  name: 'MainLayout'
+})
+
+const linksList = [
+  {
+    title: 'Home',
+    caption: '',
+    icon: 'home',
+    route: { name: 'home' }
+  }
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { api } from 'boot/axios'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+    // as linhas são os conteúdos, os dados de retorno
+    const posts = ref([])
+
+    // as colunas são os identificadores do json
+    const columns = [
+      { name: 'id', field: 'id', label: 'ID', sortable: true, align: 'left' },
+      { name: 'title', field: 'title', label: 'TITLE', sortable: true, align: 'left' },
+      { name: 'views', field: 'views', label: 'VIEWS', sortable: true, align: 'left' }
+    ]
+
+    return {
+      posts,
+      columns
+    }
+  }
+})
+</script>
